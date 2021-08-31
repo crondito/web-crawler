a0efa241df21df4a1701e70ce47f3397301b8d16
Carlos Rodas <crods57@gmail.com>
2021-08-31 11:19:10 -0500

    Gets Score

--- extract-news.js
+++ extract-news.js
@@ -12,15 +12,23 @@ request("https://news.ycombinator.com/", (error, response, body) => {
 
     let $ = cheerio.load(body);
 
-    //Clears previous news
+    // Clears previous hacker news
     fs.writeFileSync('./web-crawler/web-crawler/output/hackernews.txt', '');
 
+    // Gets the points
+    let points = $('tr').find('span.score').text().split('').filter(e => (/[0-9 ]+/).test(e)).join('');
+    let points2 = points.split(' ').map((item) => parseInt(item, 10));
+    points2.pop();
+
+    // Gets the number of comments
+
     // Gets the first 30 news from https://news.ycombinator.com/ and saves them in the output folder as hackernews.txt
-    $('tr.athing:has(td.votelinks)').each(function (index) {
-        let rank = $(this).find('span.rank').text().trim();
+    $('tr.athing').each(function (i) {
+        let rank = $(this).find('span.rank').text();
         let title = $(this).find('td.title > a').text().trim();
         //let link = $(this).find('td.title > a').attr('href');
-        fs.appendFileSync('./web-crawler/web-crawler/output/hackernews.txt', rank + title + '\n');
+        let score = points2[i];
+        fs.appendFileSync('./web-crawler/web-crawler/output/hackernews.txt', rank + ' ' + title + '\n' + 'Score: ' + score + ' |\n');
     });
 
 });
